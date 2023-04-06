496c0802c07d0065c4283bcfc9bb77e98fa9f406
rafiharefa <rafi.harefa@gmail.com>
2023-04-06 11:27:40 +0700

    update
    events_page

--- lib/app/modules/events_page/views/events_page_view.dart
+++ lib/app/modules/events_page/views/events_page_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pog/app/modules/component/footer.dart';
+import 'package:pog/app/modules/component/nav_bar.dart';
+import 'package:pog/app/modules/events_page/views/component/events_content.dart';
 
+import '../../../../app_color.dart';
 import '../controllers/events_page_controller.dart';
 
 class EventsPageView extends GetView<EventsPageController> {
@@ -9,14 +14,28 @@ class EventsPageView extends GetView<EventsPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.grey,
       appBar: AppBar(
-        title: const Text('EventsPageView'),
-        centerTitle: true,
+        title: NavBar(),
       ),
       body: Center(
-        child: Text(
-          'EventsPageView is working',
-          style: TextStyle(fontSize: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Text('Current Available Events',
+                style: GoogleFonts.montserrat(fontWeight: FontWeight.w700, fontSize: 50, color: AppColor.white),
+              ),
+
+              SizedBox(height: 30),
+
+              EventsContent(),
+              SizedBox(height: 40),
+
+              Footer()
+
+            ],
+          ),
         ),
       ),
     );
